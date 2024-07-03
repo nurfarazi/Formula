727281682aba6321c35162f559f20e0262ef00de
farazi <nur369188@gmail.com>
2024-07-03 12:22:19 +0600

    Updated: Skeleton API

---
+++ Api/Controllers/FansController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class FansController : ControllerBase
+{
+    public FansController()
+    {
+    }
+
+    [HttpGet]
+    public IActionResult GetAsync()
+    {
+        return Ok("Fans");
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetByIdAsync(int id)
+    {
+        return Ok($"Fans {id}");
+    }
+
+    [HttpPost]
+    public IActionResult PostAsync()
+    {
+        return Ok("Fans");
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult PutAsync(int id)
+    {
+        return Ok($"Fans {id}");
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteAsync(int id)
+    {
+        return Ok($"Fans {id}");
+    }
+}
